a86fb7e43789b682ed3d72f7cc4c03c9a3274b97
kashdevine <kasheen.mowatt@gmail.com>
2022-01-03 21:23:16 -0800

    docker compose templated. Model, and dbcontext spun up. Dev app settings prepared for connection string for testing db. Controllers dir cleared.

---
+++ src/Inventory/Data/InventoryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Data
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Inventory>? Inventories { get; set; }
+    }
+}
