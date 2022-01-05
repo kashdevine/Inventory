ffcf22c72b60d453d7fbcd6856433bfc4a232140
kashdevine <kasheen.mowatt@gmail.com>
2022-01-04 21:02:56 -0800

    Created brand, category, and vendor pocos. Decoupled them from the Inventory model and added them as navigation properties. Updated properties for the Inventory model to have some built in calculated props.

--- src/Inventory/Models/Inventory.cs
+++ src/Inventory/Models/Inventory.cs
@@ -4,23 +4,22 @@ namespace Inventory.Models
 {
     public class Inventory
     {
-        public Guid ID { get; set; }
+        public Guid Id { get; set; }
 
-        [Required(ErrorMessage ="Inventory Name Required")]
+        [Required(ErrorMessage ="Inventory Name Required.")]
         public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Description Required")]
+        [Required(ErrorMessage = "Description Required.")]
         public string? Description { get; set; }
+
+        [Display(Name ="Per Unit Cost")]
+        public decimal PerUnitCost { get; set; }
 
         public string? SKU { get; set; }
 
         public string? ExternalSKU { get; set; }
         
         public string? Barcode { get; set; }
-
-        public string? Brand { get; set; }
-
-        public string? Vendor { get; set; }
 
         public int Weight { get; set; }
 
@@ -32,12 +31,65 @@ namespace Inventory.Models
 
         public int Quantity { get; set; }
 
-        [Display(Name ="Per Unit Cost")]
-        public int PerUnitCost { get; set; }
-
-        public int Price { get; set; }
+        public decimal Price { get; set; }
 
         public int Variant { get; set; }
+
+        [Display(Name = "Is Digital")]
+        public bool IsDigital { get; set; }
+
+        [Display(Name = "Available Stock")]
+        public int? AvailableStock { get; set; }
+
+        [Display(Name = "Restock Threshold")]
+        public int? RestockThreshold { get; set; }
+
+        [Display(Name = "Maximum Stock Threshold")]
+        public int? MaxStockThreshold { get; set; }
+
+        [Required]
+        public DateTime Created { get; set; }
+
+        public DateTime Updated { get; set; }
+
+
+
+        public Guid BrandId { get; set; }
+        public Brand? Brand { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        public Guid VendorId { get; set; }
+        public Vendor? Vendor { get; set; }
+
+
+
+        private decimal netProfit;
+
+        public decimal NetProfit
+        {
+            get { return netProfit; }
+            private set { netProfit = this.PerUnitCost - this.Price; }
+        }
+
+
+        private bool needsReorder;
+
+        public bool NeedsReorder
+        {
+            get { return needsReorder; }
+            private set {
+                if((this.AvailableStock != null && this.RestockThreshold != null) 
+                    && (this.AvailableStock > 0 && this.RestockThreshold > 0))
+                {
+                    needsReorder = this.AvailableStock <= this.RestockThreshold;
+                }
+            }
+
+        }
+
+
 
     }
 }
---
+++ src/Inventory/Models/Vendor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Models
+{
+    public class Vendor
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="Vendor must have name.")]
+        public string? Name { get; set; }
+
+        public ICollection<Inventory>? InventoryCollection { get; set; }
+    }
+}
