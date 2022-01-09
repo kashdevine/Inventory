1e5f67a7dd2c77cc964682cb5a5ad8e56f573ec1
kashdevine <kasheen.mowatt@gmail.com>
2022-01-08 21:56:05 -0800

    Removed the test template and created first test file.

---
+++ src/InventoryTests/BrandRepositoryUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventory.Contracts;
+using Xunit;
+
+namespace InventoryTests
+{
+    public class BrandRepositoryUnitTests
+    {
+        public BrandRepositoryUnitTests()
+        {
+            // TODO create full test db using test config settings.
+        }
+    }
+}
