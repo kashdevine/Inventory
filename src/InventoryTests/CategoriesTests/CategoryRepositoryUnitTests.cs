d22479c4f32ec872c3bd0cc992d33789701a7713
kashdevine <kasheen.mowatt@gmail.com>
2022-01-21 20:46:11 -0800

    Created the unit test file for the categories.

---
+++ src/InventoryTests/CategoriesTests/CategoryRepositoryUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using Inventory.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace InventoryTests.CategoriesTests
+{
+    public class CategoryRepositoryUnitTests
+    {
+        private InventoryContext _ctx;
+
+        public CategoryRepositoryUnitTests()
+        {
+            var contextOptions = new DbContextOptionsBuilder<InventoryContext>()
+                                    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TestInventoryDb;Trusted_Connection=True;MultipleActiveResultSets=true")
+                                    .Options;
+            _ctx = new InventoryContext(contextOptions);
+        }
+    }
+}
