332fb3f87092fa29df3a528115564d20c5e1114e
kashdevine <kasheen.mowatt@gmail.com>
2022-01-23 14:20:09 -0800

    Created the vendor Repository

---
+++ src/Inventory/Contracts/IVendorRepository.cs
@@ -0,0 +1,25 @@
+﻿using Inventory.Models;
+
+namespace Inventory.Contracts
+{
+    public interface IVendorRepository
+    {
+        public Task<IEnumerable<Vendor>> GetVendors();
+
+        public Task<Vendor> GetVendorById(Guid Id);
+
+        public Task<Vendor> GetVendorByName(string Name);
+
+        public Task<Vendor> CreateVendor(Vendor Vendor);
+
+        public Task<bool> VendorDoesExist(string VendorName);
+
+        public Task<bool> VendorDoesExist(Guid VendorId);
+
+        public Task<Vendor> UpdateVendor(Vendor Vendor);
+
+        public Task<bool> DeleteVendor(Guid Id);
+
+        public Task<bool> Save();
+    }
+}
