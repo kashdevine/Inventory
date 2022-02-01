e60b93d736ba200e590350e0626b38ddb164c51a
kashdevine <kasheen.mowatt@gmail.com>
2022-01-31 21:55:27 -0800

    Created the directory and test file for the Brand v1 api endpoints.

---
+++ src/Inventory/Controllers/Api/v1/BrandsController.cs
@@ -0,0 +1,23 @@
+﻿using Inventory.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Controllers.Api.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandRepository _brandRepository;
+        public BrandsController(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+
+        //TODO: Get
+        //TODO: Get By ID
+        //TODO: Create
+        //TODO: Update
+        //TODO: Delete
+    }
+}
