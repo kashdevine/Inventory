b83b80ad9c9e135a3e9199f648e45770a8b5e2b1
kashdevine <kasheen.mowatt@gmail.com>
2022-02-01 22:38:01 -0800

    First controller unit test created. New notes added to controller.

--- src/InventoryTests/BrandTests/Api/v1/BrandsControllerTests.cs
+++ src/InventoryTests/BrandTests/Api/v1/BrandsControllerTests.cs
@@ -6,16 +6,42 @@ using System.Threading.Tasks;
 using Xunit;
 using Moq;
 using Inventory.Contracts;
+using Inventory.Models;
+using Inventory.Controllers.Api.v1;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryTests.BrandTests.Api.v1
 {
     public class BrandsControllerTests
     {
-        public BrandsControllerTests()
+
+        [Fact]
+        public async Task GetBrands_API_Returns_JsonOfBrands()
         {
+            //arrange
             var MockBrandRepo = new Mock<IBrandRepository>();
+            MockBrandRepo.Setup(br => br.GetBrands()).Returns(SeedBrandsAsync());
 
             var BrandRepo = MockBrandRepo.Object;
+
+            var _brandsController = new BrandsController(BrandRepo);
+
+            //act
+            var result = await _brandsController.GetBrands();
+
+            //assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        public static async Task<IEnumerable<Brand>> SeedBrandsAsync()
+        {
+            return new List<Brand>()
+            {
+                new Brand(){ Name = "Brand1"},
+                new Brand(){ Name = "Brand2"},
+                new Brand(){ Name = "Brand3"},
+                new Brand(){ Name = "Brand4"}
+            };
         }
     }
 }
