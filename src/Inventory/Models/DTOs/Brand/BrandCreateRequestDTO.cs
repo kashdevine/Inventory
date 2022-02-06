4b52f0faccdd5b12b5f9b40919cb2f3786fdc2ba
kashdevine <kasheen.mowatt@gmail.com>
2022-02-05 16:15:16 -0800

    Finished created the brand controller unit tests and the controller.

---
+++ src/Inventory/Models/DTOs/Brand/BrandCreateRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Models.DTOs.Brand
+{
+    public class BrandCreateRequestDTO
+    {
+        [Required(ErrorMessage = "Brand name must be provided.")]
+        public string? Name { get; set; }
+    }
+}
---
+++ src/Inventory/Models/DTOs/Brand/BrandUpdateRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Models.DTOs.Brand
+{
+    public class BrandUpdateRequestDTO
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Brand name must be provided.")]
+        public string? Name { get; set; }
+    }
+}
