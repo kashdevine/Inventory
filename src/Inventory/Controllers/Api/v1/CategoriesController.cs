2511225aba10e69a6b5c7031f6e2d62f4d3cdbd0
kashdevine <kasheen.mowatt@gmail.com>
2022-02-06 22:05:33 -0800

    Started controller api and tests for categories

---
+++ src/Inventory/Controllers/Api/v1/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Controllers.Api.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
---
+++ src/InventoryTests/CategoriesTests/Api/v1/CategoriesControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryTests.CategoriesTests.Api.v1
+{
+    public class CategoriesControllerTests
+    {
+    }
+}
