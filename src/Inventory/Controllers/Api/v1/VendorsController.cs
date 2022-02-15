df2078e5c617d35cd4ebbc21bacdfb734ac28b55
kashdevine <kasheen.mowatt@gmail.com>
2022-02-15 15:17:25 -0800

    Created the test and endpoint to get a vendor.

--- src/Inventory/Controllers/Api/v1/VendorsController.cs
+++ src/Inventory/Controllers/Api/v1/VendorsController.cs
@@ -47,9 +47,47 @@ namespace Inventory.Controllers.Api.v1
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
-        //GetByID
+
+        /// <summary>
+        /// Gets the vendor specified by the id.
+        /// </summary>
+        /// <param name="id">A Guid.</param>
+        /// <returns>The specified vendor.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<VendorGetResponseDTO>> GetVendor(Guid id)
+        {
+            try
+            {
+                _logger.LogInformation(String.Format("Attempting to get vendor for {0} with id {1}", nameof(GetVendors), id));
+                var vendorResponse =await _vendorRepository.GetVendorById(id);
+                if(vendorResponse == null)
+                {
+                    return NotFound();
+                }
+                return Ok(vendorResponse.Adapt<VendorGetResponseDTO>());
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(exception: e, String.Format("Failded to get vendor for {0} with id {1}", nameof(GetVendors), id));
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
         //Create
+        public async Task<ActionResult<VendorGetResponseDTO>> CreateVendor(VendorCreateRequestDTO createDTO)
+        {
+            throw new NotImplementedException();
+        }
+
         //Update
+        public async Task<ActionResult<VendorGetResponseDTO>> UpdateVendor(VendorUpdateRequestDTO updateDTO) { throw new NotImplementedException(); }
+
         //Delete
+        public async Task<IActionResult> DeleteVendor(Guid id) { throw new NotImplementedException(); }
     }
 }
