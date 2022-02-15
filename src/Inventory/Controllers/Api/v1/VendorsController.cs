0b51c97b875ece7f03bb49fb059ee658110689c0
kashdevine <kasheen.mowatt@gmail.com>
2022-02-14 17:14:40 -0800

    Created the test and endpoint to get the vendors.

--- src/Inventory/Controllers/Api/v1/VendorsController.cs
+++ src/Inventory/Controllers/Api/v1/VendorsController.cs
@@ -1,6 +1,8 @@
 ﻿using Inventory.Contracts;
+using Inventory.Models.DTOs.Vendor;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Mapster;
 
 namespace Inventory.Controllers.Api.v1
 {
@@ -17,7 +19,34 @@ namespace Inventory.Controllers.Api.v1
             _logger = logger;
         }
 
-        //GetAll
+        /// <summary>
+        /// Gets all vendors
+        /// </summary>
+        /// <returns>A list of vendors.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<VendorGetResponseDTO>>> GetVendors()
+        {
+            var vendors = new List<VendorGetResponseDTO>();
+
+            try
+            {
+                _logger.LogInformation(String.Format("Attempting to get vendors for {0}", nameof(GetVendors)));
+                var vendorsDb = await _vendorRepository.GetVendors();
+                foreach(var vendor in vendorsDb)
+                {
+                    vendors.Add(vendor.Adapt<VendorGetResponseDTO>());
+                }
+
+                return Ok(vendors);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(exception:e, String.Format("Failded to get vendors for {0}", nameof(GetVendors)));
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
         //GetByID
         //Create
         //Update
