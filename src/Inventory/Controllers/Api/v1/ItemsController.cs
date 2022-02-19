b3084f669e09f0408a10a9ae4ad8d90a4b405b39
kashdevine <kasheen.mowatt@gmail.com>
2022-02-19 15:28:08 -0800

    Initial setup for the item respository and controller tests.

---
+++ src/Inventory/Controllers/Api/v1/ItemsController.cs
@@ -0,0 +1,54 @@
+﻿using Inventory.Contracts;
+using Inventory.Models.DTOs.Item;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Controllers.Api.v1
+{
+    /// <summary>
+    /// API endpoints for the items.
+    /// </summary>
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemRepository _itemRepository;
+        private readonly ILogger<ItemsController> _logger;
+
+        public ItemsController(IItemRepository itemRepository, ILogger<ItemsController> logger)
+        {
+            _itemRepository = itemRepository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ItemGetResponseDTO>>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ItemGetResponseDTO>> GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ItemGetResponseDTO>> CreateItem(ItemCreateRequestDTO createDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ItemGetResponseDTO>> UpdateItem(Guid id, ItemUpdateRequestDTO updateDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/InventoryTests/ItemTests/Api/v1/ItemControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Inventory.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryTests.ItemTests.Api.v1
+{
+    [Collection("InventoryTests")]
+
+    public class ItemControllerTests
+    {
+        private InventoryContext _ctx;
+
+        public ItemControllerTests()
+        {
+            var contextOptions = new DbContextOptionsBuilder<InventoryContext>()
+            .UseSqlServer(connectionString: Utility.ConnectionString)
+            .Options;
+            _ctx = new InventoryContext(contextOptions);
+        }
+    }
+}
