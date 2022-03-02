c58d871cd14f061a9f296ccc28c6691aaed799d3
kashdevine <kasheen.mowatt@gmail.com>
2022-03-01 16:58:42 -0800

    Added new server policies in case connection with db fails.

---
+++ src/Inventory/Policies/ServerPolicy.cs
@@ -0,0 +1,33 @@
+﻿using Polly;
+using Polly.Retry;
+using System.Data.Entity.Core;
+
+namespace Inventory.Policies
+{
+    public class ServerPolicy
+    {
+        public AsyncRetryPolicy DbConnectionRetry { get; }
+        public AsyncRetryPolicy RetryDbForever { get; }
+
+        public ServerPolicy()
+        {
+            DbConnectionRetry = Policy.Handle<EntityException>().RetryAsync(
+                5, 
+                onRetry:(exception, retryCount)=>
+                    {
+                    Console.Error.WriteLine(String.Format("{0}: Attempted to connect to db {1} times and failed. Retrying..", exception, retryCount));
+                    }
+                );
+
+            RetryDbForever = Policy.Handle<EntityException>()
+                .WaitAndRetryForeverAsync(
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                onRetry: (exception, retryCount) =>
+                    {
+                    Console.Error.WriteLine(String.Format("{0}: Attempted to connect to db {1} times and failed. Retrying..", exception, retryCount));
+                    }
+                );
+        }
+
+    }
+}
